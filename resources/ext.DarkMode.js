6abf9765803936005481bda28c07050a18408e8a
WaitSpring <me@waitspring.com>
2023-01-23 20:30:49 +0800

    fix js
    Signed-off-by: WaitSpring <me@waitspring.com>

--- resources/ext.DarkMode.js
+++ resources/ext.DarkMode.js
@@ -1,26 +1,14 @@
 /* eslint-disable no-var */
+/* global mediaWiki */
 'use strict';
 
-$( function () {
-	if ( !mw.user.options.get( 'darkmode' ) ) {
-		if ( window.matchMedia( '( prefers-color-scheme: dark )' ).matches ) {
-			$( 'html' ).removeClass( 'client-lightmode' );
-			$( 'html' ).addClass( 'client-darkmode' );
-		} else if ( window.matchMedia( '( prefers-color-scheme: light)' ).matches ) {
-			$( 'html' ).removeClass( 'client-darkmode' );
-			$( 'html' ).addClass( 'client-lightmode' );
-		}
-	} else {
-		if ( mw.user.options.get( 'darkmode' ) === 1 ) {
-			$( 'html' ).removeClass( 'client-lightmode' );
-			$( 'html' ).addClass( 'client-darkmode' );
-		} else if ( mw.user.options.get( 'darkmode' ) === 0 ) {
-			$( 'html' ).removeClass( 'client-darkmode' );
-			$( 'html' ).addClass( 'client-lightmode' );
-		}
-	}
+$( function ( mw ) {
+	var darkMode = ( $( 'html' ).attr( 'class' ) ).indexOf( 'client-darkmode' ) > -1 || mw.storage.get( 'ext.DarkMode' ) === 1 ? 1 : 0;
 
-	var darkMode = ( $( 'html' ).attr( 'class' ) ).indexOf( 'client-darkmode' ) > -1 ? 1 : 0;
+	if ( !darkMode && window.matchMedia( '( prefers-color-scheme: dark )' ).matches ) {
+		$( 'html' ).removeClass( 'client-lightmode' );
+		$( 'html' ).addClass( 'client-darkmode' );
+	}
 
 	var darkModeButtonIcon = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='50' height='50' viewBox='0 0 13.229 13.229'%3E%3Ccircle cx='6.614' cy='6.614' fill='%23fff' stroke='%2336c' stroke-width='1.322' r='5.953'/%3E%3Cpath d='M6.88 11.377a4.762 4.762 0 0 1-4.125-7.144 4.762 4.762 0 0 1 4.124-2.38v4.762z' fill='%2336c' paint-order='markers stroke fill'/%3E%3C/svg%3E";
 
@@ -41,10 +29,12 @@ $( function () {
 		if ( darkMode === 0 ) {
 			$( 'html' ).removeClass( 'client-lightmode' );
 			$( 'html' ).addClass( 'client-darkmode' );
+			mw.storage.set( 'ext.DarkMode', 1 );
 			new mw.Api().saveOption( 'darkmode', 1 );
 		} else {
 			$( 'html' ).removeClass( 'client-darkmode' );
 			$( 'html' ).addClass( 'client-lightmode' );
+			mw.storage.set( 'ext.DarkMode', 0 );
 			new mw.Api().saveOption( 'darkmode', 0 );
 		}
 	} ).on( 'mouseenter mouseleave', function ( e ) {
@@ -57,4 +47,4 @@ $( function () {
 			$darkModeButton.css( 'bottom', '120px' );
 		}
 	} );
-}() );
+}( mediaWiki ) );
