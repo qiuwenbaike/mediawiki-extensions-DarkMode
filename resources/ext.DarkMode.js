2e894fe1ab1f24afda1337c43cfe07d7caae0bcc
WaitSpring <me@waitspring.com>
2023-01-23 21:24:55 +0800

    remove cookie
    Signed-off-by: WaitSpring <me@waitspring.com>

--- resources/ext.DarkMode.js
+++ resources/ext.DarkMode.js
@@ -3,14 +3,13 @@
 'use strict';
 
 $( function ( mw ) {
-	var initialDarkMode = ( $( 'html' ).attr( 'class' ) ).indexOf( 'client-darkmode' ) > -1 || $.cookie( 'darkmode' ) === 1 ? 1 : 0;
+	var darkMode = ( $( 'html' ).attr( 'class' ) ).indexOf( 'client-darkmode' ) > -1 ? 1 : 0;
 
-	if ( !initialDarkMode && window.matchMedia( '( prefers-color-scheme: dark )' ).matches ) {
+	if ( window.matchMedia( '( prefers-color-scheme: dark )' ).matches ) {
 		$( 'html' ).removeClass( 'client-lightmode' );
 		$( 'html' ).addClass( 'client-darkmode' );
 	}
 
-	var darkMode = ( $( 'html' ).attr( 'class' ) ).indexOf( 'client-darkmode' ) > -1 ? 1 : 0;
 	var darkModeButtonIcon = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='50' height='50' viewBox='0 0 13.229 13.229'%3E%3Ccircle cx='6.614' cy='6.614' fill='%23fff' stroke='%2336c' stroke-width='1.322' r='5.953'/%3E%3Cpath d='M6.88 11.377a4.762 4.762 0 0 1-4.125-7.144 4.762 4.762 0 0 1 4.124-2.38v4.762z' fill='%2336c' paint-order='markers stroke fill'/%3E%3C/svg%3E";
 
 	var $darkModeButton = $( '<img>' ).attr( {
@@ -30,14 +29,10 @@ $( function ( mw ) {
 		if ( darkMode === 0 ) {
 			$( 'html' ).removeClass( 'client-lightmode' );
 			$( 'html' ).addClass( 'client-darkmode' );
-			$.removeCookie( 'darkmode' );
-			$.cookie( 'darkmode', 1 );
 			new mw.Api().saveOption( 'darkmode', 1 );
 		} else {
 			$( 'html' ).removeClass( 'client-darkmode' );
 			$( 'html' ).addClass( 'client-lightmode' );
-			$.removeCookie( 'darkmode' );
-			$.cookie( 'darkmode', 0 );
 			new mw.Api().saveOption( 'darkmode', 0 );
 		}
 	} ).on( 'mouseenter mouseleave', function ( e ) {
