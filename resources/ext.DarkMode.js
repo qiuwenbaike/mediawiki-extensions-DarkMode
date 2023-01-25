6998a7ac2ee17d0a78245699c2e12faab28fffdb
WaitSpring <me@waitspring.com>
2023-01-25 13:03:44 +0800

    update translation
    Signed-off-by: WaitSpring <me@waitspring.com>

--- resources/ext.DarkMode.js
+++ resources/ext.DarkMode.js
@@ -6,6 +6,7 @@
 
 ( function ( $, mw ) {
 	var extensionName = 'ext.DarkMode';
+	var linkTitle, linkAlt;
 	var isDarkMode = matchMedia( '( prefers-color-scheme: dark )' ).matches;
 	var modeSwitcher = function () {
 		if ( localStorage[ extensionName ] === '0' ) {
@@ -45,7 +46,12 @@
 			}
 		}
 		if ( localStorage[ extensionName ] === '1' ) {
+			linkTitle = mw.message( 'darkmode-default-link' );
+			linkAlt = mw.message( 'darkmode-default-link-tooltip' );
 			return true;
+		} else {
+			linkTitle = mw.message( 'darkmode-link' );
+			linkAlt = mw.message( 'darkmode-link-tooltip' );
 		}
 	};
 	var darkModeButtonIcon = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='50' height='50' viewBox='0 0 13.229 13.229'%3E%3Ccircle cx='6.614' cy='6.614' fill='%23fff' stroke='%2336c' stroke-width='1.322' r='5.953'/%3E%3Cpath d='M6.88 11.377a4.762 4.762 0 0 1-4.125-7.144 4.762 4.762 0 0 1 4.124-2.38v4.762z' fill='%2336c' paint-order='markers stroke fill'/%3E%3C/svg%3E";
