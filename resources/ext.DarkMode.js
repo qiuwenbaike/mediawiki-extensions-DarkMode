9ab0a3acdc2c984d836e9d99572ed313388bbeb1
WaitSpring <me@waitspring.com>
2023-01-25 13:16:13 +0800

    remove some jquery
    Signed-off-by: WaitSpring <me@waitspring.com>

--- resources/ext.DarkMode.js
+++ resources/ext.DarkMode.js
@@ -5,89 +5,75 @@
  */
 
 ( function ( $, mw ) {
-	var extensionName = 'ext.DarkMode';
-	var linkTitle, linkAlt;
-	var isDarkMode = matchMedia( '( prefers-color-scheme: dark )' ).matches;
-	var modeSwitcher = function () {
-		if ( localStorage[ extensionName ] === '0' ) {
-			document.documentElement.classList.remove( 'client-lightmode' );
-			document.documentElement.classList.add( 'client-darkmode' );
-			localStorage.setItem( extensionName, '1' );
-			new mw.Api().saveOption( 'darkmode', '1' );
-			$darkModeButton.attr( {
-				'alt': mw.message( 'darkmode-default-link' ),
-				'title': mw.message( 'darkmode-default-link-tooltip' )
-			} );
-		} else {
-			document.documentElement.classList.remove( 'client-darkmode' );
-			document.documentElement.classList.add( 'client-lightmode' );
-			localStorage.setItem( extensionName, '0' );
-			new mw.Api().saveOption( 'darkmode', '0' );
-			$darkModeButton.attr( {
-				'alt': mw.message( 'darkmode-link' ),
-				'title': mw.message( 'darkmode-link-tooltip' )
-			} );
-		}
-	};
-	var modeObserver = {
-		dark: function ( mediaQueryList ) {
-			if ( mediaQueryList.matches && localStorage[ extensionName ] === '0' ) {
-				modeSwitcher();
+	var extensionName = 'ext.DarkMode',
+		isDarkMode = matchMedia( '( prefers-color-scheme: dark )' ).matches,
+		darkModeButtonIcon = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='50' height='50' viewBox='0 0 13.229 13.229'%3E%3Ccircle cx='6.614' cy='6.614' fill='%23fff' stroke='%2336c' stroke-width='1.322' r='5.953'/%3E%3Cpath d='M6.88 11.377a4.762 4.762 0 0 1-4.125-7.144 4.762 4.762 0 0 1 4.124-2.38v4.762z' fill='%2336c' paint-order='markers stroke fill'/%3E%3C/svg%3E",
+		$darkModeButton = $( '<img>' ).attr( {
+			src: darkModeButtonIcon,
+			id: 'darkModeButton'
+		} ).css( {
+			cursor: 'pointer',
+			opacity: 0.7,
+			position: 'fixed',
+			right: '8px',
+			'user-select': 'none',
+			width: '32px',
+			height: '32px'
+		} ).appendTo( 'body' ),
+		modeSwitcher = function () {
+			if ( localStorage[ extensionName ] === '0' ) {
+				document.documentElement.classList.remove( 'client-lightmode' );
+				document.documentElement.classList.add( 'client-darkmode' );
+				localStorage.setItem( extensionName, '1' );
+				new mw.Api().saveOption( 'darkmode', '1' );
+				$darkModeButton.attr( {
+					alt: mw.message( 'darkmode-default-link' ),
+					title: mw.message( 'darkmode-default-link-tooltip' )
+				} );
+			} else {
+				document.documentElement.classList.remove( 'client-darkmode' );
+				document.documentElement.classList.add( 'client-lightmode' );
+				localStorage.setItem( extensionName, '0' );
+				new mw.Api().saveOption( 'darkmode', '0' );
+				$darkModeButton.attr( {
+					alt: mw.message( 'darkmode-link' ),
+					title: mw.message( 'darkmode-link-tooltip' )
+				} );
 			}
 		},
-		light: function ( mediaQueryList ) {
-			if ( mediaQueryList.matches && localStorage[ extensionName ] === '1' ) {
-				modeSwitcher();
+		modeObserver = {
+			dark: function ( mediaQueryList ) {
+				if ( mediaQueryList.matches && localStorage[ extensionName ] === '0' ) {
+					modeSwitcher();
+				}
+			},
+			light: function ( mediaQueryList ) {
+				if ( mediaQueryList.matches && localStorage[ extensionName ] === '1' ) {
+					modeSwitcher();
+				}
 			}
-		}
-	};
-	var checkDarkMode = function () {
-		if ( !localStorage[ extensionName ] ) {
-			if ( isDarkMode ) {
-				localStorage.setItem( extensionName, '1' );
+		},
+		checkDarkMode = function () {
+			if ( !localStorage[ extensionName ] ) {
+				if ( isDarkMode ) {
+					localStorage.setItem( extensionName, '1' );
+				} else {
+					localStorage.setItem( extensionName, '0' );
+				}
+			}
+			if ( localStorage[ extensionName ] === '1' ) {
+				$darkModeButton.attr( {
+					alt: mw.message( 'darkmode-default-link' ),
+					title: mw.message( 'darkmode-default-link-tooltip' )
+				} );
+				return true;
 			} else {
-				localStorage.setItem( extensionName, '0' );
+				$darkModeButton.attr( {
+					alt: mw.message( 'darkmode-link' ),
+					title: mw.message( 'darkmode-link-tooltip' )
+				} );
 			}
-		}
-		if ( localStorage[ extensionName ] === '1' ) {
-			$darkModeButton.attr( {
-				'alt': mw.message( 'darkmode-default-link' ),
-				'title': mw.message( 'darkmode-default-link-tooltip' )
-			} );
-			return true;
-		} else {
-			$darkModeButton.attr( {
-				'alt': mw.message( 'darkmode-link' ),
-				'title': mw.message( 'darkmode-link-tooltip' )
-			} );
-		}
-	};
-	var darkModeButtonIcon = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='50' height='50' viewBox='0 0 13.229 13.229'%3E%3Ccircle cx='6.614' cy='6.614' fill='%23fff' stroke='%2336c' stroke-width='1.322' r='5.953'/%3E%3Cpath d='M6.88 11.377a4.762 4.762 0 0 1-4.125-7.144 4.762 4.762 0 0 1 4.124-2.38v4.762z' fill='%2336c' paint-order='markers stroke fill'/%3E%3C/svg%3E";
-
-	var $darkModeButton = $( '<img>' ).attr( {
-		src: darkModeButtonIcon,
-		id: 'darkModeButton',
-	} ).css( {
-		cursor: 'pointer',
-		opacity: 0.7,
-		position: 'fixed',
-		right: '8px',
-		'user-select': 'none',
-		width: '32px',
-		height: '32px'
-	} ).on( 'mouseenter mouseleave', function ( e ) {
-		this.style.opacity = e.type === 'mouseenter' ? 1 : 0.7;
-	} ).attr( 'draggable', 'false' ).on( 'click', function () {
-		modeSwitcher();
-	} ).appendTo( 'body' );
-
-	window.addEventListener( 'scroll', function () {
-		if ( $('#cat_a_lot').length > 0 || $('#proveit').length > 0 || $('.wordcount').length > 0 ) {
-			$darkModeButton.css( 'bottom', '162px' );
-		} else {
-			$darkModeButton.css( 'bottom', '120px' );
-		}
-	} );
+		};
 
 	matchMedia( '( prefers-color-scheme: dark )' ).addEventListener( 'change', function ( event ) {
 		modeObserver.dark( event.target );
@@ -98,6 +84,18 @@
 	window.addEventListener( 'storage', function ( event ) {
 		if ( event.key === extensionName ) {
 			modeSwitcher();
+		}
+	} );
+	$darkModeButton.on( 'mouseenter mouseleave', function ( e ) {
+		this.style.opacity = e.type === 'mouseenter' ? 1 : 0.7;
+	} ).attr( 'draggable', 'false' ).on( 'click', function () {
+		modeSwitcher();
+	} );
+	window.addEventListener( 'scroll', function () {
+		if ( document.getElementById( 'cat_a_lot' ) || document.getElementById( 'proveit' ) || document.getElementsByClassName( 'wordcount' )[ 0 ] ) {
+			$darkModeButton.css( 'bottom', '162px' );
+		} else {
+			$darkModeButton.css( 'bottom', '120px' );
 		}
 	} );
 
