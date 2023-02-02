721c90d37367bae9c60da5f08afcbb8e55b93234
WaitSpring <me@waitspring.com>
2023-02-02 22:13:25 +0800

    rename cookie
    Signed-off-by: WaitSpring <me@waitspring.com>

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -44,7 +44,7 @@ class Hooks implements
 			// On usedarkmode is set, overwrite the cookie.
 			return (bool)$var;
 		}
-        $varCookie = $context->getRequest()->getCookie( 'darkmode' );
+        $varCookie = $_COOKIE[ 'usedarkmode' ];
         if ( $varCookie === '0' || $varCookie === '1' ) {
 			// Otherwise return cookie value.
 			return (bool)$varCookie;
